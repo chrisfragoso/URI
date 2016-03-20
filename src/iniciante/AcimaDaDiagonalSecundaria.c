5ec13483b0032cef624a23b9a327ac6ab15e7612
C Fragoso <christianefsouto@hotmail.com>
2016-03-20 00:10:51 -0300

    Added files via upload

---
+++ src/iniciante/AcimaDaDiagonalSecundaria.c
@@ -0,0 +1,32 @@
+/*           UNIPETECH
+        CHRISTIANE FRAGOSO
+       CIENCIA DA COMPUTAÇÃO  */
+
+#include <stdio.h>
+
+int main(){
+    char opt;
+    int i, j;
+    double N[12][12], soma = 0.0;
+    
+    
+    scanf("%s", &opt);   
+    
+    for(i = 0; i < 12; i++){
+            for(j = 0; j < 12; j++){
+                   scanf("%lf", &N[i][j]);
+                   
+                   if((i + j) < 11){
+                        soma += N[i][j];
+                   }
+            }
+   }
+   
+   if(opt == 'S')
+         printf("%.1f\n", soma);
+    else
+         printf("%.1f\n", soma/66.0);
+    return 0;
+}
+
+
